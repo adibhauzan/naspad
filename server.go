e9b07d33b386c8520e35ad9d6e51f7eff0f0b7fe
adibhauzan <adibhauzan48@gmail.com>
2024-07-20 23:42:19 +0700

    FEAT : router

--- server.go
+++ server.go
@@ -1,1 +1,25 @@
 package naspad
+
+import (
+	"log"
+	"net/http"
+)
+
+func NewServer() *Driver {
+	driver := NewDriver()
+
+	// Set up a default RGroup with the base route path as "/"
+	driver.RGroup.baseRoutePath = "/"
+
+	driver.RGroup.BaseRoutePath()
+	// Return the Driver which implements http.Handler
+	return driver
+}
+
+// Run starts the HTTP server with the provided Driver
+func (d *Driver) Run(addr string) {
+	log.Printf("Listening and serving HTTP on %s", addr)
+	if err := http.ListenAndServe(addr, d); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
